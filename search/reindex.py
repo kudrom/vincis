8e3bd230f2ff365b83340b2b10840836789a4f09
kudrom <kudrom@lavabit.com>
2013-07-17 16:25:58 +0200

    Search completely integrated and support for email

---
+++ search/reindex.py
@@ -0,0 +1,25 @@
+import os
+from django.conf import settings
+from whoosh.index import open_dir 
+import logging
+
+logger = logging.getLogger("vincis.console")
+
+
+def delete_document(document):
+    """ Delete a document from the index """
+    ix = open_dir(settings.INDEX)
+    writer = ix.writer()
+    writer.delete_by_term("pk", document.pk)
+    writer.commit()
+
+def add_document(document):
+    """ Add a document to the index """
+    ix = open_dir(settings.INDEX)
+    writer = ix.writer()
+    tags = map(lambda x: x.title, document.tags.all())
+    writer.add_document(pk=document.pk,
+                        title=document.title, 
+                        summary=document.summary, 
+                        tags=",".join(tags))
+    writer.commit()
